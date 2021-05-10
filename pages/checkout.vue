9bc4428d8a70807bd2d7614534f6ce494c5acd2f
TaurusNova <susilojosafat@gmail.com>
2021-05-10 20:32:36 +0700

    Add Checkout Page

---
+++ pages/checkout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container px-12">
+    <section class="breadcrumbs font-semibold text-gray-400 text-xs mt-4">
+      <ul class="inline-flex mx-auto">
+        <li>
+          <nuxt-link to="/katalog">Kembali ke Katalog</nuxt-link>
+        </li>
+        <li class="mx-2">></li>
+        <li>
+          <nuxt-link to="/user/profile">Keranjang Belanja</nuxt-link>
+        </li>
+        <li class="mx-2">></li>
+        <li class="text-black">Checkout</li>
+        <li class="mx-2">></li>
+        <li>Konfirmasi</li>
+      </ul>
+    </section>
+    <div>
+      <h1 class="font-bold text-2xl text-primary mt-6">Checkout</h1>
+    </div>
+    <div class="">
+      <div class="">Detail Pengiriman</div>
+      <div class="">Pesanan Anda</div>
+    </div>
+  </div>
+</template>
